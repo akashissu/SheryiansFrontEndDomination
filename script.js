67cb94ee7c043a34306d59696a75df5fb8acc95d
akashissu <akash.kr.issu@gmail.com>
2025-08-05 11:22:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+// // var a = 15;
+// // var b = 16;
+
+// // console.log("hey My name IS Akash");
+
+// //Some Inbult Function
+
+// //console alert prompt ye thino browser ke parts Hai...
+
+
+// //console.log
+
+// console.log("Hello");
+
+
+// //console.warn
+
+// console.warn("Hello");
+
+// //console.error
+
+// console.error("Hello");
+
+
+// //alert
+
+// alert("Hello");
+
+
+// //prompt
+// //prompt alert ka bhai hai jo ki alert se ek kadam aage hai
+
+// prompt("naam Batao");
+
+
+// //Variables And Constant 
+
+// //har programming lang mein data se deal karna hota hai,wo
+// //alag alag parkar ka data ho sakta hai,ab aisa data jisse 
+// // humein deal karna hai,wo save to karna padega
+// //to wo data save karne ke lia hume koi saving spzce chahiye jaha par
+// //data save ho jayega and ye save karne ke liye unhe kuch naam
+// //bhi dena padega and issiliye humein variable
+// //and constant bananne padta hai taaki data save hojaaae
+// //and uska koi naam bhi ho taaki hum baad me unhe access kar paaye
+
+// var x = 55;
+// var y = 65;
+
+// const z = 30;
+
+
+// //if else statement
+
+// if(12>13){
+//     console.log("Hey");
+// }
+// else{
+//     console.log("Hello");
+// }
+
+
+// //loops
+
+// function abcd(){
+//     console.log("Akash");
+//     console.log("Akash");
+//     console.log("Akash");
+//     console.log("Akash");
+//     console.log("Akash");
+//     console.log("Akash");
+//     console.log("Akash");
+
+// }
+
+// abcd();
+
+// function efgd(val){
+//     console.log(12+val);
+
+// }
+// efgh(12);
+
+//----------------------Learn Selection-----------------
+
+//Selection
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// var btn = document.querySelector('button');
+
+//Selection
+
+//id
+
+// var abcd = document.querySelector('#abcd');
+
+// //class
+
+// var classabcd = document.querySelector('.abcd');
+
+// // tag
+
+// var h3 = document.querySelector("h3");
+
+
+//----------------------Modifying Element-----------------
+
+
+var btn = document.querySelector("button");
+
+btn.textContent = "Start downloading in 3 Second..."
+
+var hea = document.querySelector("h1");
+
+hea.textContent += "Helloooooo";
+
+hea.innerHTML = "<i>My Name Is Akash<i/>";
