3776164f8d35fcbc7363f1001b8c8be81f243e91
akashissu <akash.kr.issu@gmail.com>
2025-08-09 23:34:55 +0530

    ek aur naya

--- script.js
+++ script.js
@@ -327,6 +327,17 @@
 // })
 
 
+// var prg = document.querySelector('#progress');
+
+// var count = 0;
+
+// var int = setInterval(function(){
+//     if(count === 100){
+//         clearInterval(int);
+//     }
+//     count++;
+//     prg.style.width = count+'%';
+// },50)
 
 
 
