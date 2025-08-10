749743bf71658ecee19077c5f8f4e56ff56ee0ef
akashissu <akash.kr.issu@gmail.com>
2025-08-10 10:00:00 +0530

    JS

--- script.js
+++ script.js
@@ -340,6 +340,14 @@
 // },50)
 
 
+// var textarea = document.querySelector("textarea");
+// var counter = document.querySelector("#counter");
+
+
+// textarea.addEventListener("input",function(){
+//     counter.textContent = textarea.value.length;
+// })
+
 
 
 
